47af9d4ef69a7978ff0fea069e32326f58f6f742
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-04-02 22:53:37 +0200

    commit
    new saving system
    new display system

--- main.js
+++ main.js
@@ -1,126 +1,152 @@
 const prompt = require(`prompt-sync`)({ sigint: true });
 const fs = require('fs');
 const quest = require('./questions.json');
-const result = require('./result.json');
-
-let quit1 = false;
+const jsonArray = JSON.parse(fs.readFileSync('./result.json', 'utf-8'));
 const animals = ['cat', 'dog', 'rabbit', 'fish'];
-const jsonArray = [];
 
-for (let i = 0; i < result.length; i++) {
-  jsonArray.push(result[i])
-}
-
-while (!quit1) {
-  let choice1 = prompt("1. köra formulärten    2. visa alla resultat     q. avsluta programet   svar: ").trim().toLocaleLowerCase();
-  switch (choice1) {
+let main = false;
+while (!main) {
+  let choice = prompt("1. run the form    2. show all results     q. exit the program   answer: ").trim().toLocaleLowerCase();
+  switch (choice) {
     case '1':
-      let quit2 = false;
-      do {
-        let points = [0, 0, 0, 0];
-        let totalPoint = 0;
-        let name = prompt("vad heter du?: ");
-
-        for (let i = 0; i < quest.length; i++) {
-          if (i === 'stop') { break; }
-          let quit3 = false;
-          do {
-            console.log(quest[i].q);
-            let choice2 = prompt("y. ja    n. nej    q. sluta formuläret    svar: ").trim().toLocaleLowerCase();
-            switch (choice2) {
-              case 'y':
-                //caculate yes-points
-                for (let j = 0; j < points.length; j++) {
-                  points[j] += quest[i].y[j];
-                }
-                quit3 = true;
-                break;
-              case 'n':
-                //caculate no-points
-                for (let j = 0; j < points.length; j++) {
-                  points[j] += quest[i].n[j];
-                }
-                quit3 = true;
-                break;
-              case 'q':
-                //break the for-loop
-                i = 'stop';
-                quit3 = true;
-                break;
-              default:
-                //invalade value
-                console.log("valet finns inte!");
-            }
-          } while (!quit3);
-        }
-
-        //caculate totalpoints
-        totalPoint = points.reduce((prev, curr) => prev + curr, 0)
-
-        const scores = [];
-        //add object in scores-array with animal name and score in percentage
-        for (let i = 0; i < animals.length; i++) {
-          if (points[i] <= 0) {
-            scores[i] = { animal: animals[i], score: points[i].toFixed(2) };
-          } else {
-            scores[i] = { animal: animals[i], score: ((points[i] / totalPoint) * 100).toFixed(2) };
-          }
-        }
-
-        //sort the score
-        scores.sort((a, b) => b.score - a.score);
-
-        let player = {
-          'name': name,
-          'time': new Date().toLocaleString(),
-          'scores': scores
-        };
-
-        console.log(player);
-
-        jsonArray.push(player);
-
-        let quit4 = false;
-        do {
-          //input 3
-          let playAgain = prompt("1. kör formuläret igen     q. sluta spela     svar: ").trim().toLocaleLowerCase();
-          switch (playAgain) {
-            case '1':
-              quit4 = true;
-              break;
-            case 'q':
-              quit2 = true;
-              quit4 = true;
-              break;
-            default:
-              //invalade value
-              console.log("valet finns inte!");
-          }
-        } while (!quit4);
-      } while (!quit2);
-
-      //write to the file
-      fs.writeFile('./result.json', JSON.stringify(jsonArray, null, 2), (err) => {
-        if (err) throw err;
-        console.log("Successfully wrote file");
-      });
+      theForm()
       break;
     case '2':
-      //display the result
-      if (jsonArray.length === 0) {
-        console.log("det finns inge tidigre resultater!")
-      } else {
-        for (let i = 0; i < jsonArray.length; i++) {
-          console.log(jsonArray[i]);
-        }
-      }
+      displayResult()
       break;
     case 'q':
-      //quit the program
-      quit1 = true;
+      writeToJason()
+      main = true;
       break;
     default:
-      //invalade value
-      console.log("valet finns inte! 3")
+      invaladeValue()
   }
+}
+
+function theForm() {
+  let endTheForm = false;
+  while (!endTheForm) {
+    let points = [0, 0, 0, 0];
+    let totalPoint = 0;
+    let name = prompt("name: ").trim().toLocaleLowerCase();
+
+    for (let i = 0; i < quest.length; i++) {
+      if (i === 'stop') { break; }
+      let answer = false;
+      while (!answer) {
+        console.log(quest[i].q);
+        let choice2 = prompt("y. Yes    n. No    q. Stop the form    answer: ").trim().toLocaleLowerCase();
+        switch (choice2) {
+          case 'y':
+            //caculate yes-points
+            for (let j = 0; j < points.length; j++) {
+              points[j] += quest[i].y[j];
+            }
+            answer = true;
+            break;
+          case 'n':
+            //caculate no-points
+            for (let j = 0; j < points.length; j++) {
+              points[j] += quest[i].n[j];
+            }
+            answer = true;
+            break;
+          case 'q':
+            //break the for-loop
+            i = 'stop';
+            answer = true;
+            break;
+          default:
+            invaladeValue()
+        }
+      };
+    }
+
+    caculateAndPunch(points, totalPoint, name)
+
+    let endForm = false;
+    while (!endForm) {
+      let playAgain = prompt("1. run the form again     q. close the forms     answer: ").trim().toLocaleLowerCase();
+      switch (playAgain) {
+        case '1':
+          endForm = true;
+          break;
+        case 'q':
+          endTheForm = true;
+          endForm = true;
+          break;
+        default:
+          invaladeValue()
+      }
+    };
+  };
+}
+
+function caculateAndPunch(points, totalPoint, name) {
+  //caculate totalpoints
+  totalPoint = points.reduce((prev, curr) => prev + curr, 0)
+
+  const scores = [];
+  //puch object into scores-array with animal name and score in percentage
+  for (let i = 0; i < animals.length; i++) {
+    if (points[i] <= 0) {
+      scores[i] = { animal: animals[i], score: points[i].toFixed(2) };
+    } else {
+      scores[i] = { animal: animals[i], score: ((points[i] / totalPoint) * 100).toFixed(2) };
+    }
+  }
+
+  //sort the score
+  scores.sort((a, b) => b.score - a.score);
+
+  //puch to arrey
+  if (jsonArray.find(player => Object.keys(player)[0] === name)) {
+    const existingPlayer = jsonArray.find(player => Object.keys(player)[0] === name);
+    existingPlayer[name].push({
+      'time': new Date().toLocaleString(),
+      'scores': scores
+    });
+  } else {
+    let player = {};
+    player[name] = [{
+      'time': new Date().toLocaleString(),
+      'scores': scores
+    }];
+    jsonArray.push(player);
+  }
+
+  //console the result
+  console.log(name + " : ");
+  scores.forEach(scoreObject => {
+    console.log(JSON.stringify(scoreObject));
+  });
+}
+
+function displayResult() {
+  if (jsonArray.length === 0) {
+    console.log("There are no previous results!");
+  } else {
+    for (let i = 0; i < jsonArray.length; i++) {
+      for (let key in jsonArray[i]) {
+        console.log(`${key} :`);
+        if (jsonArray[i].hasOwnProperty(key)) {
+          for (let j = 0; j < jsonArray[i][key].length; j++) {
+            console.log(`Time: ${jsonArray[i][key][j].time}`)
+            console.table(jsonArray[i][key][j].scores);
+          }
+        }
+      }
+    }
+  }
+}
+
+function invaladeValue() {
+  console.log("the choice does not exist!")
+}
+
+function writeToJason() {
+  fs.writeFile('./result.json', JSON.stringify(jsonArray, null, 2), (err) => {
+    if (err) throw err;
+    console.log("Successfully wrote file");
+  });
 }
